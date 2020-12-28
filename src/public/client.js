06e3dbd3705f483d474b8cef1faaffff52f5bbec
Harry Beckwith <harrybeckwith@harrys-mbp.lan>
2020-12-28 10:10:44 +0000

    map to get photos per rover name

--- src/public/client.js
+++ src/public/client.js
@@ -1,23 +1,61 @@
 let store = {
-  user: { name: "Student" },
-  apod: "",
   rovers: ["Curiosity", "Opportunity", "Spirit"]
 };
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-// ------------------------------------------------------  API CALLS
+// add our markup to the page
+const root = document.getElementById("root");
+
+const updateStore = (store, newState) => {
+  store = Object.assign(store, newState);
+  render(root, store);
+};
+
+const render = async (root, state) => {
+  root.innerHTML = App(state);
+};
+
+// create content
+const App = state => {
+  let { rovers, apod } = state;
+
+  return `
+      <header></header>
+      <main>
+          ${Greeting(store.user.name)}
+          <section>
+              <h3>Put things on the page!</h3>
+              <p>Here is an example section.</p>
+              <p>
+                  One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
+                  the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
+                  This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
+                  applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
+                  explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
+                  but generally help with discoverability of relevant imagery.
+              </p>
+              ${ImageOfTheDay(apod)}
+          </section>
+      </main>
+      <footer></footer>
+  `;
+};
+
+const formattRoverGallery = arr => {
+  return arr.map(item => item.img_src);
+};
 
 const getRoverInfo = async roverName => {
   try {
     await fetch(`http://localhost:3000/roverInfo/${roverName}`)
       .then(res => res.json())
       .then(roverInfo => {
-        console.log(roverInfo);
+        const photos = formattRoverGallery(roverInfo.roverData.photos);
+        console.log(photos);
       });
   } catch (error) {
-    console.log("error:", err);
+    console.log("error:", error);
   }
 };
 
